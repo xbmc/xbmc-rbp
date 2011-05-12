594dea9b7319efb07a4ae16cef2344d28fc36d25
theuni <theuni-nospam-@xbmc.org>
2011-05-12 01:50:35 -0400

    [sigma] correctly set build type

--- tools/sigma/sigma-sdk.mk
+++ tools/sigma/sigma-sdk.mk
@@ -1,13 +1,14 @@
 #Set This. It should be {/path/to/OE}/trunk/build/tmp/sysroots/
 export XBMCSTAGE=
 
+export BUILD=$(shell uname -m)-linux
 export PREFIX=${XBMCSTAGE}/mips-linux-gnu/usr
 export PKG_CONFIG_SYSROOT_DIR=${XBMCSTAGE}/mips-linux-gnu
 export CFLAGS=-isystem${PREFIX}/include
 export CXXFLAGS=${CFLAGS}
 export CPPFLAGS=${CFLAGS}
 export LDFLAGS=-L${XBMCSTAGE}/mips-linux-gnu/lib -Wl,-rpath-link,${XBMCSTAGE}/mips-linux-gnu/lib -L${PREFIX}/lib -Wl,-rpath-link,${PREFIX}/lib -Wl,-O1
-export CROSSBIN=${XBMCSTAGE}/x86_64-linux/usr/bin/mips-linux-gnu-
+export CROSSBIN=${XBMCSTAGE}/$(BUILD)/usr/bin/mips-linux-gnu-
 export CC=${CROSSBIN}gcc -march=mips32r2 -mtune=24kf -EL
 export CXX=${CROSSBIN}g++ -march=mips32r2 -mtune=24kf -EL -fpermissive
 export LD=${CROSSBIN}ld
@@ -17,7 +18,6 @@ export STRIP=${CROSSBIN}strip
 export OBJDUMP=${CROSSBIN}objdump
 export ACLOCAL=aclocal -I ${PREFIX}/share/aclocal -I ${PREFIX}/share/aclocal-1.11
 export HOST=mips-linux-gnu
-export BUILD=$(shell bash -c "echo $$HOSTTYPE-linux")
 export CXXCPP=${CXX} -E
 export PKG_CONFIG_LIBDIR=${PREFIX}/lib/pkgconfig
 export TARGETFS=${XBMCSTAGE}/targetfs
