abdd9788d9ba9678827b7b38a032af962abbdf89
davilla <davilla@xbmc.org>
2011-09-15 14:43:59 -0400

    initial depends

---
+++ tools/bcm/depends/depends.mk
@@ -0,0 +1,23 @@
+export HOST=arm-bcm2708-linux-gnueabi
+export BUILD=i686-linux
+export PREFIX=${XBMCPREFIX}
+export TARGETFS
+export SYSROOT=/usr/local/bcm-gcc/arm-bcm2708-linux-gnueabi/sys-root
+export RLINK_PATH=-Wl,-rpath-link,${SYSROOT}/lib -Wl,-rpath-link,${TARGETFS}/lib -Wl,-rpath-link,${TARGETFS}/usr/lib -Wl,-rpath-link,${TARGETFS}/opt/vc/lib
+
+export CFLAGS=-isystem${XBMCPREFIX}/include -isystem${SDKSTAGE}/usr/include -isystem${SDKSTAGE}/opt/vc/include -isystem${SDKSTAGE}/opt/vc
+export CFLAGS+=-L${XBMCPREFIX}/lib -L${SYSROOT}/lib -L${TARGETFS}/lib -L${TARGETFS}/usr/lib -L${TARGETFS}/opt/vc/lib ${RLINK_PATH}
+export CXXFLAGS=${CFLAGS}
+export CPPFLAGS=${CFLAGS}
+export LDFLAGS=${RLINK_PATH} -L${TARGETFS}/lib -L${TARGETFS}/usr/lib -L${XBMCPREFIX}/lib
+export LD=${TOOLCHAIN}/bin/${HOST}-ld
+export AR=${TOOLCHAIN}/bin/${HOST}-ar
+export CC=${TOOLCHAIN}/bin/${HOST}-gcc
+export CXX=${TOOLCHAIN}/bin/${HOST}-g++
+export CXXCPP=${CXX} -E
+export RANLIB=${TOOLCHAIN}/bin/${HOST}-ranlib
+export STRIP=${TOOLCHAIN}/bin/${HOST}-strip
+export OBJDUMP=${TOOLCHAIN}/bin/${HOST}-objdump 
+export ACLOCAL=aclocal -I ${PREFIX}/share/aclocal -I ${TARGETFS}/usr/share/aclocal-1.11
+export PKG_CONFIG_LIBDIR=${PREFIX}/lib/pkgconfig:${SDKSTAGE}/lib/pkgconfig:${SDKSTAGE}/usr/lib/pkgconfig
+export PATH:=${PREFIX}/bin:$(PATH):${TOOLCHAIN}/bin
---
+++ tools/bcm/setup-sdk.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+SCRIPT_PATH=$(cd `dirname $0` && pwd)
+
+#Edit these two
+BUILDROOT=/opt/bcm-rootfs
+TARBALLS=/opt/xbmc-tarballs
+
+XBMCPREFIX=/opt/xbmc-bcm
+SDKSTAGE=$BUILDROOT
+TARGETFS=$BUILDROOT
+TOOLCHAIN=/usr/local/bcm-gcc
+#
+sudo mkdir -p $XBMCPREFIX
+sudo chmod 777 $XBMCPREFIX
+mkdir -p $XBMCPREFIX/lib
+mkdir -p $XBMCPREFIX/include
+
+echo "SDKSTAGE=$SDKSTAGE"                                              >  $SCRIPT_PATH/Makefile.include
+echo "XBMCPREFIX=$XBMCPREFIX"                                          >> $SCRIPT_PATH/Makefile.include
+echo "TARGETFS=$TARGETFS"                                              >> $SCRIPT_PATH/Makefile.include
+echo "TOOLCHAIN=$TOOLCHAIN"                                            >> $SCRIPT_PATH/Makefile.include
+echo "BUILDROOT=$BUILDROOT"                                            >> $SCRIPT_PATH/Makefile.include
+echo "BASE_URL=http://mirrors.xbmc.org/build-deps/darwin-libs"         >> $SCRIPT_PATH/Makefile.include
+echo "TARBALLS_LOCATION=$TARBALLS"                                     >> $SCRIPT_PATH/Makefile.include
+echo "RETRIEVE_TOOL=/usr/bin/curl"                                     >> $SCRIPT_PATH/Makefile.include
+echo "RETRIEVE_TOOL_FLAGS=-Ls --create-dirs --output \$(TARBALLS_LOCATION)/\$(ARCHIVE)" >> $SCRIPT_PATH/Makefile.include
+echo "ARCHIVE_TOOL=/bin/tar"                                           >> $SCRIPT_PATH/Makefile.include
+echo "ARCHIVE_TOOL_FLAGS=xf"                                           >> $SCRIPT_PATH/Makefile.include
+echo "JOBS=$((`grep -c processor /proc/cpuinfo` -1))"                  >> $SCRIPT_PATH/Makefile.include
