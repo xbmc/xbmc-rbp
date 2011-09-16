2c21a32822be99d879527a30fbe705e889e2275d
davilla <davilla@xbmc.org>
2011-09-16 19:53:02 -0400

    [gles] clean up egl configAttrs

--- xbmc/windowing/egl/WinBindingEGL.cpp
+++ xbmc/windowing/egl/WinBindingEGL.cpp
@@ -93,22 +93,9 @@ bool CWinBindingEGL::CreateWindow(EGLNativeDisplayType nativeDisplay, EGLNativeW
         EGL_BLUE_SIZE,       8,
         EGL_ALPHA_SIZE,      8,
         EGL_SURFACE_TYPE,    EGL_WINDOW_BIT,
-        EGL_NONE
-  };
-/*
-  EGLint configAttrs[] = {
-        EGL_RED_SIZE,        8,
-        EGL_GREEN_SIZE,      8,
-        EGL_BLUE_SIZE,       8,
-        EGL_DEPTH_SIZE,     16,
-        EGL_STENCIL_SIZE,    0,
-        EGL_SAMPLE_BUFFERS,  0,
-        EGL_SAMPLES,         0,
-        EGL_SURFACE_TYPE,    EGL_WINDOW_BIT,
         EGL_RENDERABLE_TYPE, EGL_OPENGL_ES2_BIT,
         EGL_NONE
   };
-*/
 
   // Find out how many configurations suit our needs  
   eglStatus = eglChooseConfig(m_display, configAttrs, NULL, 0, &configCount);
