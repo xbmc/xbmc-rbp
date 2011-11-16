51747f67c6b6f7b622b8e14b79608a56a342a883
huceke <gimli@i7.(none)>
2011-11-16 21:55:56 +0100

    [rbp] fixed toolchain search for aclocal

--- tools/rbp/depends/depends.mk
+++ tools/rbp/depends/depends.mk
@@ -18,6 +18,6 @@ export CXXCPP=${CXX} -E
 export RANLIB=${TOOLCHAIN}/bin/${HOST}-ranlib
 export STRIP=${TOOLCHAIN}/bin/${HOST}-strip
 export OBJDUMP=${TOOLCHAIN}/bin/${HOST}-objdump 
-export ACLOCAL=aclocal -I ${PREFIX}/share/aclocal -I ${TARGETFS}/usr/share/aclocal-1.11
+export ACLOCAL=aclocal -I ${SDKSTAGE}/usr/share/aclocal -I ${TARGETFS}/usr/share/aclocal-1.11
 export PKG_CONFIG_LIBDIR=${PREFIX}/lib/pkgconfig:${SDKSTAGE}/lib/pkgconfig:${SDKSTAGE}/usr/lib/pkgconfig
 export PATH:=${PREFIX}/bin:$(PATH):${TOOLCHAIN}/bin
