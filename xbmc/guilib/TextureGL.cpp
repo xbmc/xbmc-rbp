aed757052e9bd725d9cf7b9c943aeb87da09e724
huceke <gimli@i7.(none)>
2012-01-15 23:10:49 +0100

    [rbp] removed old debug message

--- xbmc/guilib/TextureGL.cpp
+++ xbmc/guilib/TextureGL.cpp
@@ -133,8 +133,6 @@ void CGLTexture::LoadToGPU()
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
-    { GLenum err = glGetError(); CLog::Log(LOGERROR, "GL ERROR: %i", (int)err);}
-
     VerifyGLState();
     m_loadedToGPU = true;
     return;
@@ -258,8 +256,6 @@ void CGLTexture::LoadToGPU()
   }
   glTexImage2D(GL_TEXTURE_2D, 0, internalformat, m_textureWidth, m_textureHeight, 0,
     pixelformat, GL_UNSIGNED_BYTE, m_pixels);
-
-  { GLenum err = glGetError(); CLog::Log(LOGERROR, "GL ERROR: %i", (int)err);}
 #endif
   VerifyGLState();
 
