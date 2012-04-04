cf7aff0f3b7aaf6ea2b363c7d984af010b849b69
huceke <gimli@i7.(none)>
2012-04-04 20:30:42 +0200

    [rbp] changed enable airplay server

--- tools/rbp/depends/depends.mk
+++ tools/rbp/depends/depends.mk
@@ -4,13 +4,13 @@ ifeq ($(USE_BUILDROOT),1)
 	export BUILD=i686-linux
 	export PREFIX=$(XBMCPREFIX)
 	export SYSROOT=$(BUILDROOT)/output/host/usr/arm-unknown-linux-gnueabi/sysroot
-	export CFLAGS+=-isystem$(SYSROOT)/usr/include -isystem$(SYSROOT)/opt/vc/include -isystem$(PREFIX)/include -isystem$(PREFIX)/usr/include/mysql
-	export CXXFLAGS=$(CFLAGS)
-	export CPPFLAGS=$(CFLAGS)
+	export CFLAGS+=-isystem$(SYSROOT)/usr/include -isystem$(SYSROOT)/opt/vc/include -isystem$(PREFIX)/include -isystem$(PREFIX)/usr/include/mysql --sysroot=$(SYSROOT)
+	export CXXFLAGS=$(CFLAGS) --sysroot=$(SYSROOT)
+	export CPPFLAGS=$(CFLAGS) --sysroot=$(SYSROOT)
 	export LDFLAGS=-L$(SYSROOT)/opt/vc/lib -L$(XBMCPREFIX)/lib
-	export LD=$(TOOLCHAIN)/bin/$(HOST)-ld --sysroot=$(SYSROOT)
-	export CC=$(TOOLCHAIN)/bin/$(HOST)-gcc --sysroot=$(SYSROOT)
-	export CXX=$(TOOLCHAIN)/bin/$(HOST)-g++ --sysroot=$(SYSROOT)
+	export LD=$(TOOLCHAIN)/bin/$(HOST)-ld 
+	export CC=$(TOOLCHAIN)/bin/$(HOST)-gcc 
+	export CXX=$(TOOLCHAIN)/bin/$(HOST)-g++ 
 	export OBJDUMP=$(TOOLCHAIN)/bin/$(HOST)-objdump
 	export RANLIB=$(TOOLCHAIN)/bin/$(HOST)-ranlib
 	export STRIP=$(TOOLCHAIN)/bin/$(HOST)-strip
